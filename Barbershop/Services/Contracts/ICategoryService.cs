b182a7cbe894182b9b7a5277179314647334e989
andre <andre@DESKTOP-PK0OSO6>
2025-07-08 17:57:36 +0300

    Add Category Service

---
+++ Barbershop/Services/Contracts/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using Barbershop.ViewModels;
+using Barbershop.Data.Models;
+
+namespace Barbershop.Services.Contracts
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryViewModel>> GetAllAsync();
+        Task<Category?> GetByIdAsync(int id);
+    }
+}
