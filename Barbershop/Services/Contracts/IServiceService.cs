8f00f8d87193caa9002b344250f72a82470a7078
andre <andre@DESKTOP-PK0OSO6>
2025-07-08 17:58:37 +0300

    Add Services Service

---
+++ Barbershop/Services/Contracts/IServiceService.cs
@@ -0,0 +1,11 @@
+﻿using Barbershop.Data.Models;
+using Barbershop.ViewModels;
+
+namespace Barbershop.Services.Contracts
+{
+    public interface IServiceService
+    {
+        Task<IEnumerable<ServiceViewModel>> GetAllAsync();
+        Task<Service?> GetByIdAsync(int id);
+    }
+}
