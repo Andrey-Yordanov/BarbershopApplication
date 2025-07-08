c6d3766fef95af13cd08ab9a96a183d702eabf85
andre <andre@DESKTOP-PK0OSO6>
2025-07-08 17:58:08 +0300

    Add Contact Service

---
+++ Barbershop/Services/Contracts/IContactService.cs
@@ -0,0 +1,11 @@
+﻿using Barbershop.Data.Models;
+using Barbershop.ViewModels;
+
+namespace Barbershop.Services.Contracts
+{
+    public interface IContactService
+    {
+        Task<IEnumerable<ContactMessageViewModel>> GetAllAsync();
+        Task SendMessageAsync(ContactMessageInputModel model);
+    }
+}
