ae46b5cb2d27e45e3ea3503a1cc7cb6eefbe5f08
andre <andre@DESKTOP-PK0OSO6>
2025-07-08 17:57:54 +0300

    Implement Category Service

---
+++ Barbershop/Services/CategoryService.cs
@@ -0,0 +1,31 @@
+﻿using Barbershop.Data;
+using Barbershop.Data.Models;
+using Barbershop.Services.Contracts;
+using Barbershop.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Barbershop.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly BarbershopDbContext dbContext;
+
+        public CategoryService(BarbershopDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<IEnumerable<CategoryViewModel>> GetAllAsync()
+        {
+            return await dbContext.Categories
+            .Select(c => new CategoryViewModel
+            {
+                Id = c.Id,
+                Name = c.Name,
+            })
+            .ToListAsync();
+        }
+
+        public async Task<Category?> GetByIdAsync(int id)
+        => await dbContext.Categories.FindAsync(id);
+    }
+}
