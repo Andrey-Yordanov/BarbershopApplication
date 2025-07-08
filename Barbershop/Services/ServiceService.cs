c338d8940748d163265018e813fd53c3e8d4b104
andre <andre@DESKTOP-PK0OSO6>
2025-07-08 17:58:54 +0300

    Implement Services Service

---
+++ Barbershop/Services/ServiceService.cs
@@ -0,0 +1,36 @@
+﻿using Barbershop.Data;
+using Barbershop.Data.Models;
+using Barbershop.Services.Contracts;
+using Barbershop.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Barbershop.Services
+{
+    public class ServiceService : IServiceService
+    {
+        private readonly BarbershopDbContext dbContext;
+
+        public ServiceService(BarbershopDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<ServiceViewModel>> GetAllAsync()
+        {
+            return await dbContext.Services
+                .Include(s => s.Category)
+                .Select(s => new ServiceViewModel
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    Price = s.Price,
+                    Duration = s.DurationInMinutes,
+                    CategoryName = s.Category.Name
+                })
+                .ToListAsync();
+        }
+
+        public async Task<Service?> GetByIdAsync(int id)
+            => await dbContext.Services.FindAsync(id);
+    }
+}
