1c2f663af1adf378d7f423b47daf260328c04134
andre <andre@DESKTOP-PK0OSO6>
2025-07-08 17:59:31 +0300

    changed the Duration property type

--- Barbershop/ViewModels/ServiceViewModel.cs
+++ Barbershop/ViewModels/ServiceViewModel.cs
@@ -5,10 +5,10 @@
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public decimal Price { get; set; }
-        public TimeSpan Duration { get; set; }
+        public int Duration { get; set; }
         public string CategoryName { get; set; } = null!;
 
         public string PriceFormatted => $"{Price:C}";
-        public string DurationFormatted => $"{(int)Duration.TotalMinutes} мин";
+        public string DurationFormatted => $"{Duration} мин";
     }
 }
