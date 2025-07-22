1c8b8fe29c8681e159823af8c5618c9c687d88a3
andre <andre@DESKTOP-PK0OSO6>
2025-07-22 15:34:24 +0300

    add admin and user roles

---
+++ Barbershop/Data/AdminSeeder.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Barbershop.Data
+{
+    public static class AdminSeeder
+    {
+        public static async Task SeedAdminAsync(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string adminRole = "Admin";
+            if (!await roleManager.RoleExistsAsync(adminRole))
+            {
+                await roleManager.CreateAsync(new IdentityRole(adminRole));
+            }
+
+            string adminEmail = "admin@barbershop.com";
+            string adminPassword = "admin123";
+
+            var existingAdmin = await userManager.FindByEmailAsync(adminEmail);
+            if (existingAdmin == null)
+            {
+                var admin = new IdentityUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    EmailConfirmed = true
+                };
+
+                var result = await userManager.CreateAsync(admin, adminPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, adminRole);
+                }
+            }
+        }
+    }
+}
