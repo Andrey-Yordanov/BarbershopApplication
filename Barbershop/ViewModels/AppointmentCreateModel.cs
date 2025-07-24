128bf789f7e48d8ae9d5b5de1eb87dceaf37d9f3
andre <andre@DESKTOP-PK0OSO6>
2025-07-24 16:45:58 +0300

    Add time in the AppointmentCreateViewModel

--- Barbershop/ViewModels/AppointmentCreateModel.cs
+++ Barbershop/ViewModels/AppointmentCreateModel.cs
@@ -1,9 +1,14 @@
-﻿namespace Barbershop.ViewModels
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Barbershop.ViewModels
 {
     public class AppointmentCreateModel
     {
         public string UserId { get; set; } = null!;
         public int ServiceId { get; set; }
         public DateTime AppointmentDate { get; set; }
+
+        [NotMapped]
+        public string Time { get; set; } = null!;
     }
 }
