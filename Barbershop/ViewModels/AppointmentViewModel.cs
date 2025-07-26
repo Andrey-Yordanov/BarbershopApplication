9106ad9604080475f6831f3241a4881135d5a3b6
andre <andre@DESKTOP-PK0OSO6>
2025-07-26 16:47:20 +0300

    Username added in viewModel

--- Barbershop/ViewModels/AppointmentViewModel.cs
+++ Barbershop/ViewModels/AppointmentViewModel.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string ServiceName { get; set; } = null!;
         public DateTime AppointmentDate { get; set; }
+        public string Username { get; set; } = null!;
 
         public string DateFormatted => AppointmentDate.ToString("dd.MM.yyyy HH:mm");
     }
