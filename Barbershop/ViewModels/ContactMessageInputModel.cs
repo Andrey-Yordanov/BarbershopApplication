c7c005b5990e29080167a0a9da0218782da8bf92
andre <andre@DESKTOP-PK0OSO6>
2025-07-30 17:03:19 +0300

    Added validations

--- Barbershop/ViewModels/ContactMessageInputModel.cs
+++ Barbershop/ViewModels/ContactMessageInputModel.cs
@@ -1,9 +1,21 @@
-﻿namespace Barbershop.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+using static Barbershop.Common.EntityValidationConstants.ContactMessage;
+
+namespace Barbershop.ViewModels
 {
     public class ContactMessageInputModel
     {
+        [Required]
+        [StringLength(ContactNameMaxLength, MinimumLength = ContactNameMinLength)]
         public string Name { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(ContactEmailMaxLength, MinimumLength = ContactEmailMinLength)]
         public string Email { get; set; } = null!;
+
+        [Required]
+        [StringLength(ContactMessageMaxLength, MinimumLength = ContactMessageMinLength)]
         public string Message { get; set; } = null!;
     }
 }
