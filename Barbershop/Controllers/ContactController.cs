b1b048e674be981ca88cd682d08f443f6a21750f
andre <andre@DESKTOP-PK0OSO6>
2025-07-30 17:03:05 +0300

    Changed admin contoller location

---
+++ Barbershop/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using Barbershop.Services.Contracts;
+using Barbershop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Barbershop.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactService contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            this.contactService = contactService;
+        }
+
+        [HttpGet]
+        public IActionResult Send()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Send(ContactMessageInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await contactService.SendMessageAsync(model);
+            TempData["SuccessMessage"] = "Съобщението беше изпратено успешно!";
+            return RedirectToAction("Send");
+        }
+    }
+}
---
+++ Barbershop/Controllers/_AdminController.cs
@@ -0,0 +1,45 @@
+﻿//using Barbershop.Services.Contracts;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace Barbershop.Controllers
+//{
+//    [Authorize(Roles = "Admin")]
+//    public class _AdminController : Controller
+//    {
+//        private readonly IAppointmentService appointmentService;
+//        private readonly IContactService contactService;
+
+//        public _AdminController(IAppointmentService appointmentService, IContactService contactService)
+//        {
+//            this.appointmentService = appointmentService;
+//            this.contactService = contactService;
+//        }
+
+//        [HttpGet]
+//        public async Task<IActionResult> AllAppointments(DateTime? date)
+//        {
+//            if (date == null)
+//            {
+//                var uniqueDates = await appointmentService.GetAllDatesWithAppointmentsAsync();
+//                return View("AppointmentDates", uniqueDates);
+//            }
+
+//            var normalizedDate = DateTime.SpecifyKind(date.Value.Date, DateTimeKind.Unspecified);
+
+//            Console.WriteLine($"[DEBUG] Normalized date: {normalizedDate} | Kind: {normalizedDate.Kind}");
+
+//            var appointments = await appointmentService.GetByDateAsync(normalizedDate);
+//            ViewBag.SelectedDate = normalizedDate.ToString("yyyy-MM-dd");
+
+//            return View("AppointmentsByDate", appointments);
+//        }
+
+//        [HttpGet]
+//        public async Task<IActionResult> AllMessages()
+//        {
+//            var messages = await contactService.GetAllAsync();
+//            return View("AllMessages", messages);
+//        }
+//    }
+//}
