c1fb7492b30c8d17d167ac0e034ea3af9cab0b56
andre <andre@DESKTOP-PK0OSO6>
2025-07-31 18:26:43 +0300

    Removed unnecessary controler

---
+++ Barbershop/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Barbershop.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            Console.WriteLine($"[ERROR] Status code received: {statusCode}");
+
+            return statusCode switch
+            {
+                404 => View("Error404"),
+                500 => View("Error500"),
+                _ => View("General")
+            };
+        }
+    }
+}
